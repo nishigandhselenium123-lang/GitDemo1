ebeb7015f636db60041c79c34bb0585d4e7d183c
nishigandhselenium123-lang <nishigandhselenium123@gmail.com>
2025-12-19 00:44:39 +0530

    changes by Uk developer

--- src/basicpro/ab.java
+++ src/basicpro/ab.java
@@ -9,10 +9,17 @@ public class ab {
 		int c=a+b;
 		System.out.println(c);
 	}
-
+	public static void sub()
+	{
+		int a=20;
+		int b=30;
+		int c=a-b;
+		System.out.println(c);
+	}
 	public static void main(String[] args) {
 		
 		add();
+		sub();
 		
 
 

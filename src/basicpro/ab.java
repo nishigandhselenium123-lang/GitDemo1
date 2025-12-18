921f9d8c2fad4931f849e239284a1a233a390d17
nishigandhselenium123-lang <nishigandhselenium123@gmail.com>
2025-12-18 23:38:54 +0530

    First commit By Indian Developer

---
+++ src/basicpro/ab.java
@@ -0,0 +1,22 @@
+package basicpro;
+
+public class ab {
+	
+	public static void add()
+	{
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		
+		add();
+		
+
+
+
+	}
+
+}
